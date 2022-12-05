5a7a7cfdeb79e7e6f92b33697313e97e8c4142cd
Alena <alenademy7@gmail.com>
2022-12-05 12:39:17 +0500

    Добавила решение 2 задачи

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int a, b;
+ Console.WriteLine("Введите первое число : ");
+ a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+ b = Convert.ToInt32(Console.ReadLine());
+ if (a > b)
+ {
+        Console.WriteLine("max={0}", a);
+ }
+ else
+ {
+      Console.WriteLine("max={0}", b);
+ }
+      Console.ReadLine();

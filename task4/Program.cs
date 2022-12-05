3d15ae39f79e6da0b3383dd7d377c3db5dfa4623
Alena <alenademy7@gmail.com>
2022-12-05 12:59:31 +0500

    Добавила решение 4 задачи

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿int a, b, c;
+Console.WriteLine("Введите первое число : ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число : ");
+c = Convert.ToInt32(Console.ReadLine());
+int[] num = { a, b, c };
+Console.WriteLine(Math.Max(a, Math.Max(b, c)));
+Console.ReadLine();
